612ed62f68bdb02dd72ba5159e6ff76a66d18ed2
Vincent Arel-Bundock <varel@umich.edu>
2013-07-16 23:41:01 -0400

    stata results

--- README.md
+++ README.md
@@ -18,4 +18,36 @@ The file ``examples/qreg_example.jl`` shows how to use the functions provided he
 
 We are interested in the relationship between income and expenditures on food for a sample of working class Belgian households in 1857 (the Engel data), and plot regression estimates at different quantiles.
 
+    # Load stuff
+    using QReg
+    url = "http://vincentarelbundock.github.io/Rdatasets/csv/quantreg/engel.csv"
+    dat = readtable("engel.csv")
+
+    # Fit least absolute deviation model (quantile =.5)
+    > using QReg
+    > res = qreg(:(foodexp~income), dat, .5)
+    > summary(res)
+
+    2x5 DataFrame:
+            Estimate Std.Error t value     2.5%    97.5%
+    [1,]     81.4823   14.6345 5.56783  52.7987  110.166
+    [2,]    0.560181 0.0131756 42.5164 0.534356 0.586005
+
+The results look pretty close to Stata 12's ``qreg``:
+
+    . insheet using engel.csv
+    . qreg foodexp income, vce(iid, kernel(epan2))
+    Median regression                                    Number of obs =       235
+      Raw sum of deviations 46278.06 (about 582.54126)
+      Min sum of deviations 17559.93                     Pseudo R2     =    0.6206
+
+    ------------------------------------------------------------------------------
+         foodexp |      Coef.   Std. Err.      t    P>|t|     [95% Conf. Interval]
+    -------------+----------------------------------------------------------------
+          income |   .5601805   .0131763    42.51   0.000     .5342206    .5861403
+           _cons |   81.48233   14.63518     5.57   0.000     52.64815    110.3165
+    ------------------------------------------------------------------------------
+
+We can also compute and plot (using Julia's Winston) results for various quantiles. Full code to produce the figure is in the examples folder.
+
 ![](https://github.com/vincentarelbundock/QReg/raw/master/examples/qreg_example_plot.png)
--- examples/qreg_example.jl
+++ examples/qreg_example.jl
@@ -2,7 +2,7 @@ using GLM, Winston, QReg
 
 # Load data
 url = "http://vincentarelbundock.github.io/Rdatasets/csv/quantreg/engel.csv"
-dat = readtable("/Users/USERNAME/Downloads/engel.csv")
+dat = readtable("engel.csv")
 
 # Fit least absolute deviation model (quantile =.5)
 res = qreg(:(foodexp~income), dat, .5)
@@ -29,3 +29,30 @@ setattr(p, "title", "Quantile regression: Food Expenditure ~ Income\n Red bar: O
 setattr(p, "xlabel", "Quantiles")
 setattr(p, "ylabel", "Coefficient on Income")
 file(p, "/Users/USERNAME/qreg_example_plot.png")
+
+# > summary(res)
+# 2x5 DataFrame:
+#         Estimate Std.Error t value     2.5%    97.5%
+# [1,]     81.4823   14.6345 5.56783  52.7987  110.166
+# [2,]    0.560181 0.0131756 42.5164 0.534356 0.586005
+# insheet using engel.csv
+# qreg foodexp income, vce(iid, kernel(epan2))
+ 
+# . qreg foodexp income, vce(iid, kernel(epan2))
+# Iteration  1:  WLS sum of weighted deviations =  18051.196
+# 
+# Iteration  1: sum of abs. weighted deviations =  18035.127
+# Iteration  2: sum of abs. weighted deviations =  17815.249
+# Iteration  3: sum of abs. weighted deviations =  17567.085
+# Iteration  4: sum of abs. weighted deviations =  17559.932
+# 
+# Median regression                                    Number of obs =       235
+#   Raw sum of deviations 46278.06 (about 582.54126)
+#   Min sum of deviations 17559.93                     Pseudo R2     =    0.6206
+# 
+# ------------------------------------------------------------------------------
+#      foodexp |      Coef.   Std. Err.      t    P>|t|     [95% Conf. Interval]
+# -------------+----------------------------------------------------------------
+#       income |   .5601805   .0131763    42.51   0.000     .5342206    .5861403
+#        _cons |   81.48233   14.63518     5.57   0.000     52.64815    110.3165
+# ------------------------------------------------------------------------------
