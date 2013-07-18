dbdbe01ab448e2e6d7be971428af79ce312c85a3
Vincent Arel-Bundock <varel@umich.edu>
2013-07-17 20:28:56 -0400

    post JMW comment removal

--- src/QuantileRegression.jl
+++ src/QuantileRegression.jl
@@ -1,30 +1,22 @@
 # Author: Vincent Arel-Bundock (w/ changes by JMW)
 # Contact: varel@umich.edu
-# Date: 2013-07-13
 # License: BSD-3
 # Original code from the python statsmodels project
 # https://github.com/statsmodels/statsmodels
 
-# Inject dependency into caller
 using DataFrames
 
 module QuantileRegression
-    # Need to export names you want caller to have access to.
-    # Everything else is hidden to keep the Main namespace pure.
     export qreg
 
-    # Access dependencies within module
     using DataFrames, Distributions
 
-    # I like having types at the front, but that's just my taste
     type QRegModel
         beta::Vector{Float64}
         vcov::Matrix{Float64}
         mf::ModelFrame
     end
 
-    # I like annotating function arguments to express meaning of parameters
-    #  and to enforce correct function usage
     function bandwidth_hall_sheather(n::Integer, q::Real, alpha::Real)
         z = quantile(Normal(), q)
         num = 1.5 * pdf(Normal(), z)^2
@@ -33,8 +25,7 @@ module QuantileRegression
         return h
     end
 
-    # Often better to start with a function that operates in-place
-    #  then define a wrapper that allocates memory on each call
+    # operate in-place to save on memory allocation
     function kernel_epanechnikov!(u::Vector, x::Vector)
         for i in 1:length(x)
             if abs(x[i]) <= 1
@@ -65,25 +56,15 @@ module QuantileRegression
         return m
     end
 
-    # I really like using keyword arguments for defaults so that
-    # function callers can modify them if need be.
     function qreg_coef(y::Vector, X::Matrix, q::Real = 0.5;
                        tol::Real = 1e-12, max_iter::Integer = 1_000,
                        threshold::Real = 1e-5)
-        # What is this?
         n, p = size(X)
-
-        # Allocate memory without spending time filling it
-        beta0 = Array(Float64, p)
-        beta = Array(Float64, p)
-
-        # Keep a copy of xstar that we can reuse
         xstar = copy(X)
-
-        # This ensures the loop runs at least once
         diff = Inf
 
-        # Reuse this memory
+        beta0 = Array(Float64, p)
+        beta = Array(Float64, p)
         xtx = Array(Float64, p, p)
         xty = Array(Float64, p)
         xbeta = Array(Float64, n)
@@ -91,39 +72,17 @@ module QuantileRegression
 
         for itr in 1:max_iter
             if diff > tol
-                # Copy contents of beta into beta0 without allocating new space
                 copy!(beta0, beta)
 
-                # Compute without allocating memory
-                # Note that this function name breaks Julia convention because
-                # it should have an exclamation mark in it
                 At_mul_B(xtx, xstar, X)
-                # If you were using new memory, the idiomatic form would be
-                # xtx = xstar' * X
-
-                # Compute without allocating memory
-                # Note that this function name breaks Julia convention because
-                # it should have an exclamation mark in it
                 At_mul_B(xty, xstar, y)
-                # If you were using new memory, the idiomatic form would be
-                # xty = xstar' * y
-
-                # Instead of using pinv(), it's much more efficient to use
-                # backslash. Sadly, most computer code that inverts a matrix
-                # is numerically unstable.
-                # The one thing that sucks is that I don't know how to do this
-                # in place
                 beta = xtx \ xty
-
-                # In-place computation
                 A_mul_B(xbeta, X, beta)
+
                 for i in 1:n
                     @inbounds resid[i] = y[i] - xbeta[i]
                 end
-                # Slower, not in-place, idiomatic version
-                # resid = y - X * beta
 
-                # Changed to be in-place
                 for i in 1:n
                     if abs(resid[i]) < threshold
                         @inbounds resid[i] = sign(resid[i]) * threshold
@@ -135,9 +94,7 @@ module QuantileRegression
                     end
                 end
 
-                # In-place
                 broadcast!(/, xstar, X, resid)
-                # xstar = broadcast(/, X, resid)
 
                 diff = maxdiff(beta0, beta)
             end
