60409775498b4d09abea03083ee443465cd480e8
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2017-01-02 11:17:11 +0100

    Hot-fix examples.

--- examples/qreg_and_ip_ex.jl
+++ examples/qreg_and_ip_ex.jl
@@ -1,10 +1,11 @@
 using QuantileRegression
 using DataFrames
-Data = readtable("engel.csv")
+cd(Pkg.dir("QuantileRegression")*"/examples")
+df = readtable("engel.csv")
 
 
-out_ip = qreg(foodexp~income, Data; method = :ip) # or just qreg(foodexp~income, Data)
-out_irls = qreg(foodexp~income, Data; method = :irls)
+out_ip = qreg(foodexp~income, df; method = :ip) # or just qreg(foodexp~income, df)
+out_irls = qreg(foodexp~income, df; method = :irls)
 
 println("...... Interior point ......")
 println(out_ip)
