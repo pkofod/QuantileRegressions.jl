1e2e5a18aff5efc38d1debb608c9f9a4bcbc2d30
azev77 <azev77@users.noreply.github.com>
2021-06-08 07:46:41 -0400

    Update qreg_example.jl (#45)

--- examples/qreg_example.jl
+++ examples/qreg_example.jl
@@ -1,4 +1,4 @@
-using GLM, QuantileRegressions, CSV, Winston, StatsBase
+using GLM, QuantileRegressions, CSV, Winston, StatsBase, DataFrames
 
 # Load data
 url = "http://vincentarelbundock.github.io/Rdatasets/csv/quantreg/engel.csv"
@@ -18,7 +18,10 @@ PlotDF = DataFrame(
     Y = DataPlot[:,1],
     ols = ols[:,1],
     Ymin = DataPlot[:,1] - 1.96 * DataPlot[:,2],
-    Ymax = DataPlot[:,1] + 1.96 * DataPlot[:,2])
+    Ymax = DataPlot[:,1] + 1.96 * DataPlot[:,2],
+    YminLM = ols[:,1] - 1.96 * ols[:,2],
+    YmaxLM = ols[:,1] + 1.96 * ols[:,2]
+)
 
 x = PlotDF[:X]
 p = FramedPlot()
@@ -26,10 +29,12 @@ p = FramedPlot()
 βols = Curve(x, PlotDF[:ols] , "color", "red")
 ci_low  = Curve(x, PlotDF[:Ymin], "type" , "dash")
 ci_high = Curve(x, PlotDF[:Ymax], "type" , "dash")
+ci_lowLM  = Curve(x, PlotDF[:YminLM], "type" , "dash", "color", "red")
+ci_highLM = Curve(x, PlotDF[:YmaxLM], "type" , "dash", "color", "red")
 setattr(βτ,"label","β(τ)")
 setattr(βols, "label", "β(ols)")
 lgnd = Legend(.8,.2,[βτ, βols])
-add(p, βτ, βols, ci_low, ci_high, lgnd)
+add(p, βτ, βols, ci_low, ci_high, ci_lowLM, ci_highLM, lgnd)
 setattr(p, "title" , "Quantile regression: Food Expenditure ~ Income")
 setattr(p, "xlabel", "Quantiles")
 setattr(p, "ylabel", "Coefficient on Income")
